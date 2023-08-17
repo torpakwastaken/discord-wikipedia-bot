eee43f55e95d99ab5df02723d66a795b5495cc02
torpakwastaken <yunusemrekoyunlu@gmail.com>
2023-08-17 04:45:06 +0300

    first commit

---
+++ wikipediabot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import wikipediaapi
+
+# Set up intents.
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+
+# Set up the bot with intents. Change '!' if you like any other symbol.
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online)
+    print(f'Logged in as {bot.user.name}')
+
+
+@bot.command()
+async def wiki(ctx, *, query):
+    try:
+        wiki_wiki = wikipediaapi.Wikipedia(
+            language='en',
+            extract_format=wikipediaapi.ExtractFormat.WIKI,
+            user_agent='MyDiscordBot/1.0'
+        )
+        page = wiki_wiki.page(query)
+
+        response = f"**{page.title}**\n{page.fullurl}"
+        await ctx.send(response)
+    except wikipediaapi.exceptions.DisambiguationError as e:
+        await ctx.send("Ambiguous query. Please provide more specific search terms.")
+    except wikipediaapi.exceptions.PageError as e:
+        await ctx.send("No results found for the given query.")
+
+# Run the bot
+bot.run('YOUR DISCORD BOT TOKEN')
